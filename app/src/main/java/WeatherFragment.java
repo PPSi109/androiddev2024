519caa8494e1e4cd05459f233e77c895f325a9a5
PPSi109 <SiPP.22bi13990@usth.edu.vn>
2024-09-13 15:35:45 +0700

    labwork 6

--- app/src/main/java/WeatherFragment.java
+++ app/src/main/java/WeatherFragment.java
@@ -1,3 +1,31 @@
-public class WeatherFragment {
+package vn.edu.usth.usthweather;
 
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+
+
+    public WeatherFragment() {
+    }
+
+
+    public static WeatherFragment newInstance(String param1, String param2) {
+        WeatherFragment fragment = new WeatherFragment();
+        Bundle args = new Bundle();
+        args.putString("param1", param1);
+        args.putString("param2", param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
 }
